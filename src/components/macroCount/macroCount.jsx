17433d336487d2e97f8e57e248e317b10b794b56
ColineC <coline.cantelou@gmail.com>
2024-03-18 15:59:49 +0100

    test

--- src/components/macroCount/macroCount.jsx
+++ src/components/macroCount/macroCount.jsx
@@ -1,9 +1,12 @@
 import React, { PureComponent, useEffect, useState } from 'react';
-import userApi from '../../api/userApi'
 import './macroCount.css'
 
 function macroCount(params) {
-    
+    return (
+        <>
+        test
+        </>
+    )
 }
 
 export default macroCount
